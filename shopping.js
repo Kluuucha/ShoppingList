275f59b35562245ac9d372a702cc5840392915b2
Sebastian Kobiela <sebastian.kobiela97@gmail.com>
2021-09-01 23:28:34 +0200

    Reworking loops + applying literals

--- shopping.js
+++ shopping.js
@@ -8,11 +8,11 @@ function createItemElement (item) {
     let element = document.createElement('div');
     element.classList.add('form-check');
     element.classList.add('shopping-element');
-    element.id = 'item-' + item.id;
+    element.id = `item-${item.id}`;
 
     let checkbox = document.createElement('input');
     checkbox.classList.add('form-check-input');
-    checkbox.id = 'checkbox-' + element.id;
+    checkbox.id = `checkbox-${element.id}`;
     checkbox.setAttribute('type', 'checkbox');
 
     let label = document.createElement('label');
@@ -28,17 +28,12 @@ function createItemElement (item) {
 }
 
 function clearChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
+    parent.innerHTML = '';
 }
 
 function rebuildList(){
     clearChildNodes(elementList);
-//TODO: Figure out if the loop can be done by itemList.forEach
-    for (i = 0; i < itemList.length; i++) {
-        elementList.appendChild(createItemElement(itemList[i]));
-      } 
+    itemList.forEach(item => elementList.appendChild(createItemElement(item)))
 }
 
 function getName(){
@@ -46,6 +41,7 @@ function getName(){
     newItemField.value = '';
     return name;
 }
+
 function addItemToList(){
     let submitedItem = {
         'id': itemList.length,
@@ -57,7 +53,7 @@ function addItemToList(){
 function submitItem () {
     addItemToList();
     rebuildList();
-    console.log('Current item count: '+itemList.length);
+    console.log(`Current item count: ${itemList.length}`);
 }
 
 newItemButton.addEventListener('click', submitItem);
